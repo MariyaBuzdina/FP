e204b08b85508d65e384c3ead24a4259ff150854
Mariya Buzdina <mariyabuzdina@yandex.ru>
2018-11-24 00:47:39 +0300

    Add files via upload

--- hw5.hs
+++ hw5.hs
@@ -1,3 +1,4 @@
+import Data.Monoid 
 import Data.Char
 import Data.List
 
@@ -12,7 +13,6 @@ cyrcShiftL n xs | n > 0 = cyrcShiftL (n-1) (tail xs)++[head xs]
 indices :: [a] -> [(Integer, a)]
 indices xs = zip  [1..toInteger(length xs)] xs 
 --2.2 "Обнуляет" элементы данного списка, неудовлетворяющие заданному условию
-
 --2.3
 triplewiseSum :: [Integer] -> [Integer] -> [Integer] -> [Integer]
 triplewiseSum xs ys zs = zipWith3 (\x y z-> x + y + z) xs ys zs
@@ -26,3 +26,22 @@ revRange (a, b) = unfoldr g b
 --4
 seriesK :: Int -> [Rational]
 seriesK k = iterate (/toRational k) 1
+----------------------------------------------------------------------
+--5 Сделайте сортированный список представителем класса Monoid (операция — слияние списков)
+newtype SortedList a = SortedList { getSorted :: [a] } deriving (Eq, Show)
+
+instance Ord a => Semigroup (SortedList a) where
+  SortedList xs <> SortedList ys = SortedList $ mergeSortedLists xs ys
+instance Ord a => Monoid (SortedList a) where
+  mempty = SortedList []
+  mappend = (<>)
+--слияние отсортированных несписков
+mergeSortedLists :: Ord a => [a] -> [a] -> [a]
+mergeSortedLists xs [] = xs
+mergeSortedLists [] ys = ys
+mergeSortedLists (x:xs) (y:ys) | (x <= y) = x : (mergeSortedLists xs (y:ys))
+			       | otherwise = y : (mergeSortedLists (x:xs) ys)
+
+
+
+
